6efece067c0a08a58113e06d2cf11b0c09f0250b
Antonio Vanegas <hpsaturn@gmail.com>
2017-06-13 22:37:41 -0500

    fix wifi enable flow and suspend flow

--- src/Driver.cpp
+++ src/Driver.cpp
@@ -30,6 +30,9 @@ const char *password     = WIFI_PASS;
 // Initialize the OLED display using Wire library
 SSD1306  display(0x3c, 5, 4);
 
+#define MAX_RECONNECT 1
+bool isWifiEnable = false;
+int reconnect = 0;
 int threshold = 80;
 bool touch2detected = false;
 bool touch3detected = false;
@@ -55,7 +58,13 @@ void setup() {
   display.flipScreenVertically();
   display.setContrast(255);
   display.clear();
-  Serial.println("OLED ready..");
+  Serial.println("OLED ready");
+
+  // Init touch callbacks
+  touchAttachInterrupt(T2, gotTouch2, threshold);
+  touchAttachInterrupt(T3, gotTouch3, threshold);
+  Serial.println("Buttons ready");
+
 
   display.setFont(ArialMT_Plain_10);
   display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
@@ -66,64 +75,69 @@ void setup() {
   WiFi.mode(WIFI_STA);
   WiFi.begin (ssid, password);
 
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    display.clear();
-    display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2 - 10, "Wifi Setup Failed!\nRebooting..");
-    display.display();
-    delay(3000);
-    ESP.restart();
+  while (WiFi.waitForConnectResult() != WL_CONNECTED && reconnect<MAX_RECONNECT) { 
+    Serial.print("Connection Failed! try to reconnect:");
+    Serial.println(reconnect);
+    reconnect++;
   }
-  Serial.println("WiFi ready..");
-  display.clear();
+  reconnect=0;
 
-  if (!MDNS.begin("esp32potp")) {
-    Serial.println("Error setting up MDNS responder!");
-    display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2 - 10, "MDNS Setup Failed!\nRebooting..");
+  if(WiFi.isConnected()){
+    Serial.println("WiFi ready");
+    isWifiEnable=true;
+    display.clear();
+
+    if (!MDNS.begin("esp32potp")) {
+      Serial.println("Error setting up MDNS responder!");
+      display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2 - 10, "MDNS Setup Failed!\nPress B to reboot");
+      display.display();
+      delay(3000);
+    }
+
+    ArduinoOTA.begin();
+    ArduinoOTA.setHostname("esp32potp");
+    ArduinoOTA.onStart([]() {
+        Serial.println("ArduinoOTA starting..");
+        display.clear();
+        display.setFont(ArialMT_Plain_10);
+        display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
+        display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2 - 10, "OTA Update");
+        display.display();
+    });
+
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        display.drawProgressBar(4, 32, 120, 6, progress / (total / 100) );
+        display.display();
+    });
+
+    ArduinoOTA.onEnd([]() {
+        Serial.println("OTA complete.");
+        display.clear();
+        display.setFont(ArialMT_Plain_10);
+        display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
+        display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2, "Restart");
+        display.display();
+    });
+
+    // Align text vertical/horizontal center
+    display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2, "Ready for OTA:\n" + WiFi.localIP().toString());
     display.display();
-    delay(3000);
-    ESP.restart();
+    Serial.println("OTA ready");
   }
-
-  Serial.println("OTA updates..");
-  ArduinoOTA.begin();
-  ArduinoOTA.onStart([]() {
-    Serial.println("ArduinoOTA starting..");
+  else{
+    Serial.println("WiFi setup failed!");
+    isWifiEnable = false;
     display.clear();
-    display.setFont(ArialMT_Plain_10);
-    display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
-    display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2 - 10, "OTA Update");
+    display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2 - 10, "Wifi Setup Failed!\nPress B to reboot");
     display.display();
-  });
-
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    display.drawProgressBar(4, 32, 120, 6, progress / (total / 100) );
-    display.display();
-  });
-
-  ArduinoOTA.onEnd([]() {
-    Serial.println("OTA complete.");
-    display.clear();
-    display.setFont(ArialMT_Plain_10);
-    display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
-    display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2, "Restart");
-    display.display();
-  });
-
-  // Align text vertical/horizontal center
-  display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
-  display.setFont(ArialMT_Plain_10);
-  display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2, "Ready for OTA:\n" + WiFi.localIP().toString());
-  display.display();
-
-  // Init touch callbacks
-  Serial.println("Touch callback..");
-  touchAttachInterrupt(T2, gotTouch2, threshold);
-  touchAttachInterrupt(T3, gotTouch3, threshold);
+  }
   Serial.println("== Setup ready ==");
 }
 
-void hibernate(){
+void suspend(){
+  Serial.println("Process Suspend..");
   touch2count=0;
   display.clear();
   display.setFont(ArialMT_Plain_10);
@@ -141,18 +155,19 @@ void hibernate(){
 }
 
 void reboot(){
+  Serial.println("Process Reboot..");
   touch3count=0;
   ESP.restart();
 }
 
 void loop() {
 
-  ArduinoOTA.handle();
+  if(isWifiEnable)ArduinoOTA.handle();
 
   if(touch2detected){
     touch2detected = false;
     Serial.println("Touch 2 (GPIO2) detected");
-    if(touch2count++>7)hibernate();
+    if(touch2count++>7)suspend();
   }
   if(touch3detected){
     touch3detected = false;
@@ -160,9 +175,10 @@ void loop() {
     if(touch3count++>5)reboot();
   }
 
-  if(poweroff++>3000000){
+  if(poweroff++>6000000){
+    Serial.println("Auto Power Off");
     poweroff=0;
-    hibernate();
+    suspend();
   }
 
 }
