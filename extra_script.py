69b6a454fa0994ee7ed198ea56061c93d374060a
Antonio Vanegas <hpsaturn@gmail.com>
2018-04-01 01:34:29 -0500

    fix env var issue on lint over atom IDE

--- extra_script.py
+++ extra_script.py
@@ -7,5 +7,5 @@ defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
 import subprocess
 gitrevcount = subprocess.Popen(['git', 'rev-list', '--all', '--count', 'HEAD'], stdout=subprocess.PIPE).stdout.readline()
 print ('git revision: '+gitrevcount)
-env.Replace(VERSION="rev%s" % gitrevcount)
+env.Replace(SRC_REV="rev%s" % gitrevcount)
 # env.Replace(PROGNAME="firmware_%s" % defines.get("VERSION"))
