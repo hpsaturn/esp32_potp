79c5f867b8c6c52b7214ceb5541ff1d7cc644b9e
Antonio Vanegas <hpsaturn@gmail.com>
2018-09-06 22:15:40 -0500

    remove bad header and added vcode ignore files

--- src/WiFiManager.h
+++ src/WiFiManager.h
@@ -1,8 +1,3 @@
-/*
-  Morse.h - Library for flashing Morse code.
-  Created by David A. Mellis, November 2, 2007.
-  Released into the public domain.
-*/
 #ifndef WifiManager_h
 #define WifiManager_h
 
